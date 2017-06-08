df3fe5c5085747416372bb0cbfc09ae55ef03d25
Elaine Kiyumi <kijapinha@gmail.com>
2017-06-08 20:19:23 -0300

    feyman

---
+++ Tecnica_Feyman/Tecnica_Feyman/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace Tecnica_Feyman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.novoAssuntoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.meusAssuntusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Número = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Assunto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoAssuntoToolStripMenuItem,
+            this.meusAssuntusToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(685, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // novoAssuntoToolStripMenuItem
+            // 
+            this.novoAssuntoToolStripMenuItem.Name = "novoAssuntoToolStripMenuItem";
+            this.novoAssuntoToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.novoAssuntoToolStripMenuItem.Text = "Novo Assunto";
+            this.novoAssuntoToolStripMenuItem.Click += new System.EventHandler(this.novoAssuntoToolStripMenuItem_Click);
+            // 
+            // meusAssuntusToolStripMenuItem
+            // 
+            this.meusAssuntusToolStripMenuItem.Name = "meusAssuntusToolStripMenuItem";
+            this.meusAssuntusToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.meusAssuntusToolStripMenuItem.Text = "Meus Assuntos";
+            this.meusAssuntusToolStripMenuItem.Click += new System.EventHandler(this.meusAssuntusToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Assunto para Estudar";
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(12, 55);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(330, 26);
+            this.txt1.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(370, 52);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(79, 29);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(15, 131);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt2.Size = new System.Drawing.Size(431, 270);
+            this.txt2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Escreva sobre o tema:";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(437, 306);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(12, 412);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(140, 29);
+            this.btnSalvar.TabIndex = 7;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(309, 412);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(140, 29);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Número,
+            this.Assunto});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(437, 372);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Número
+            // 
+            this.Número.HeaderText = "Número";
+            this.Número.Name = "Número";
+            // 
+            // Assunto
+            // 
+            this.Assunto.HeaderText = "Assunto";
+            this.Assunto.Name = "Assunto";
+            this.Assunto.Width = 300;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.checkBox5);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.checkBox4);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.checkBox3);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(455, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(218, 306);
+            this.panel2.TabIndex = 10;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(3, 283);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(74, 17);
+            this.checkBox5.TabIndex = 21;
+            this.checkBox5.Text = "Dominado";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(3, 251);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(210, 26);
+            this.textBox5.TabIndex = 20;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(3, 228);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(74, 17);
+            this.checkBox4.TabIndex = 19;
+            this.checkBox4.Text = "Dominado";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(3, 196);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(210, 26);
+            this.textBox4.TabIndex = 18;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 173);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(74, 17);
+            this.checkBox3.TabIndex = 17;
+            this.checkBox3.Text = "Dominado";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(3, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(210, 26);
+            this.textBox3.TabIndex = 16;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(2, 119);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(74, 17);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "Dominado";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(2, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 26);
+            this.textBox2.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 64);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Dominado";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 26);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(460, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Pontos Principais";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Técnica Feyman";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem novoAssuntoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem meusAssuntusToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Número;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Assunto;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Tecnica_Feyman/Tecnica_Feyman/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tecnica_Feyman
+{
+    public partial class Form1 : Form
+    {
+        int assuntos = 1, i = 0;
+        static int j = -1;
+        int indice = -1;
+        List<Assuntos> meusAssuntos = new List<Assuntos>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Process pStart = new Process();
+            string link = "https://www.google.com.br/?gws_rd=cr&ei=sWo0WfmrJoKWwATqmbqQBg#q=" + txt1.Text;
+            pStart.StartInfo = new ProcessStartInfo(@link);
+            pStart.Start();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if(indice != -1)
+            {
+                if (txt1.Text != "")
+                {
+                    Assuntos assunto = new Assuntos(txt1.Text, txt2.Text, salvarPontos());
+                    meusAssuntos[indice] = assunto;
+                    limparCampos();
+                    indice = -1;
+                }
+            }
+            if(txt1.Text != "")
+            {
+                Assuntos assunto = new Assuntos(txt1.Text, txt2.Text, salvarPontos());
+                dataGridView1.Rows.Insert(i, assuntos++, assunto.assunto, assunto.sobre);
+                i++;
+                meusAssuntos.Add(assunto);
+                limparCampos();
+            }
+        }
+
+        private List<Pontos> salvarPontos()
+        {
+            List<Pontos> listaPonto = new List<Pontos>();
+            if(textBox1.Text != "")
+            {
+                listaPonto.Add(new Pontos(textBox1.Text, checkBox1.Checked));
+            }
+            if (textBox2.Text != "")
+            {
+                listaPonto.Add(new Pontos(textBox2.Text, checkBox2.Checked));
+            }
+            if (textBox3.Text != "")
+            {
+                listaPonto.Add(new Pontos(textBox3.Text, checkBox3.Checked));
+            }
+            if (textBox4.Text != "")
+            {
+                listaPonto.Add(new Pontos(textBox4.Text, checkBox4.Checked));
+            }
+            if (textBox5.Text != "")
+            {
+                listaPonto.Add(new Pontos(textBox5.Text, checkBox5.Checked));
+            }
+
+            return listaPonto;
+        }
+
+        private void meusAssuntusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EsconderComponentes();
+            dataGridView1.Visible = true;
+        }
+
+        private void EsconderComponentes()
+        {
+            foreach(Control c in Controls)
+            {
+                c.Visible = false;
+            }
+            menuStrip1.Visible = true;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void novoAssuntoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExibirComponentes();
+        }
+
+        private void ExibirComponentes()
+        {
+            foreach(Control c in Controls)
+            {
+                c.Visible = true;
+            }
+            dataGridView1.Visible = false;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int j = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString()) -1;
+            indice = j;
+            txt1.Text = meusAssuntos[j].assunto;
+            txt2.Text = meusAssuntos[j].sobre;
+            try
+            {
+                for(int z = 0; z <meusAssuntos[j].listaPontos.Count; z++)
+                {
+                    switch (z)
+                    {
+                        case 0:
+                            textBox1.Text = meusAssuntos[j].listaPontos[0].ponto;
+                            checkBox1.Checked = meusAssuntos[j].listaPontos[0].dominado;
+                            break;
+                        case 1:
+                            textBox2.Text = meusAssuntos[j].listaPontos[1].ponto;
+                            checkBox2.Checked = meusAssuntos[j].listaPontos[1].dominado;
+                            break;
+                        case 2:
+                            textBox3.Text = meusAssuntos[j].listaPontos[2].ponto;
+                            checkBox3.Checked = meusAssuntos[j].listaPontos[2].dominado;
+                            break;
+                        case 3:
+                            textBox4.Text = meusAssuntos[j].listaPontos[3].ponto;
+                            checkBox4.Checked = meusAssuntos[j].listaPontos[3].dominado;
+                            break;
+                        case 4:
+                            textBox5.Text = meusAssuntos[j].listaPontos[4].ponto;
+                            checkBox5.Checked = meusAssuntos[j].listaPontos[4].dominado;
+                            break;
+                    }
+                }
+                ExibirComponentes();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            ExibirComponentes();
+            
+        }
+
+        private void limparCampos()
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+
+        }
+    }
+
+    class Assuntos
+    {
+        public Assuntos(string assunto, string sobre, List<Pontos> listaPontos)
+        {
+            this.assunto = assunto;
+            this.sobre = sobre;
+            this.listaPontos = listaPontos;
+        }
+        public string assunto;
+        public string sobre;
+        public List<Pontos> listaPontos = new List<Pontos>();
+    }
+
+    class Pontos
+    {
+        public Pontos(string ponto, bool dominado)
+        {
+            this.ponto = ponto;
+            this.dominado = dominado;
+        }
+
+        public string ponto;
+        public bool dominado;
+    }
+}
